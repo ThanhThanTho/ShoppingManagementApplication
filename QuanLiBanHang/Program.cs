08608fc2a86b1788fcbfaa084a620801eb7fb7c3
Thanh <thanhthantho14@gmail.com>
2023-01-06 12:10:40 +0700

    last update

--- QuanLiBanHang/Program.cs
+++ QuanLiBanHang/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLiBanHang
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DangNhap()); 
+            Application.Run(new DangNhap());
         }
     }
 }
